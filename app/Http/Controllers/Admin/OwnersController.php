1d75d937c17781618abf4329b54f9d7293c52e59
hidavicent <h.iida@vicent.co.jp>
2022-10-04 17:31:44 +0900

    index, create

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -31,8 +31,8 @@ class OwnersController extends Controller
         echo $date_now->year;
         echo $date_parse;
 
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name', 'created_at')->get();
         // $q_first = DB::table('owners')->select('name')->first();
 
         // $c_test = collect([
@@ -42,8 +42,10 @@ class OwnersController extends Controller
         // var_dump($q_first);
 
         // dd($e_all, $q_get, $q_first, $c_test);
+        $owners = Owner ::select('name','email', 'created_at')->get();
+
         return view('admin.owners.index',
-        compact('e_all','q_get'));
+        compact('owners'));
     }
 
     /**
@@ -54,6 +56,7 @@ class OwnersController extends Controller
     public function create()
     {
         //
+        return view('admin.owners.create');
     }
 
     /**
